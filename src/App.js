601b977949c94dd0f1e9d0a5cbf0005ce949d304
Amanda Chiu <amanda87006611@gmail.com>
2022-03-06 14:00:11 +0800

    react-init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Member_login from './pages/member/Member_login';
 
 function App() {
   return (
     <Router>
       <>
         <Switch>
-          <Route path="/member_login">
-            <Member_login/>
-          </Route>
+          <Route path="/index-video "></Route>
+          <Route path="/index "></Route>
+          <Route path="/forum-index "></Route>
+          <Route path="/index-postPage "></Route>
+          <Route path="/index-postPage "></Route>
+          <Route path="/member-login "></Route>
+          <Route path="/member-person "></Route>
+          <Route path="/member-password "></Route>
+          <Route path="/member-address-new "></Route>
+          <Route path="/member-love "></Route>
+          <Route path="/member-address "></Route>
+          <Route path="/member-history-detail "></Route>
+          <Route path="/travel "></Route>
+          <Route path="/travel-choose "></Route>
+          <Route path="/travel-notes "></Route>
+          <Route path="/travel-notes "></Route>
+          <Route path="/ticket-order "></Route>
+          <Route path="/ticket-trip-choose "></Route>
+          <Route path="/ticket-date-choose "></Route>
+          <Route path="/ticket-meal-choose "></Route>
+          <Route path="/ticket-confirm "></Route>
+          <Route path="/coustomize-mainpage "></Route>
+          <Route path="/coustomize-suit "></Route>
+          <Route path="/coustomize-craft "></Route>
+          <Route path="/coustomize-seat "></Route>
+          <Route path="/trans-mainpage "></Route>
+          <Route path="/trans-map "></Route>
+          <Route path="/trans-input "></Route>
+          <Route path="/products-seat "></Route>
+          <Route path="/products-list "></Route>
+          <Route path="/products-detail "></Route>
+          <Route path="/cart-payment "></Route>
+          <Route path="/cart-information "></Route>
+          <Route path="/cart-final-check "></Route>
+          <Route path="/cart-credit-card "></Route>
+          <Route path="/cart-complete "></Route>
         </Switch>
       </>
     </Router>
