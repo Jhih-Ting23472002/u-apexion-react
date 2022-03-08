5abe2c33f28b8a9bbe918c69befc40fccd333e57
Albert <95019007+jack510010@users.noreply.github.com>
2022-03-08 13:55:27 +0800

    大家的Route路徑後面多了一個空格，幫大家刪掉了。

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/navbar';
 import CoustomizeMainPage from './pages/Coustomize/CoustomizeMainPage';
+import CartChoosePayment from './pages/Cart/CartChoosePayment';
 import { useState } from 'react';
 
 //頁面元件
@@ -13,45 +14,47 @@ function App() {
       <>
         <Navbar />
         <Switch>
-          <Route path="/index-video "></Route>
-          <Route path="/index "></Route>
-          <Route path="/forum-index "></Route>
-          <Route path="/index-postPage "></Route>
-          <Route path="/index-postPage "></Route>
-          <Route path="/member-login "></Route>
-          <Route path="/member-signup "></Route>
-          <Route path="/member-person "></Route>
-          <Route path="/member-password "></Route>
-          <Route path="/member-address "></Route>
-          <Route path="/member-address-new "></Route>
-          <Route path="/member-love "></Route>
+          <Route path="/index-video"></Route>
+          <Route path="/index"></Route>
+          <Route path="/forum-index"></Route>
+          <Route path="/index-postPage"></Route>
+          <Route path="/index-postPage"></Route>
+          <Route path="/member-login"></Route>
+          <Route path="/member-signup"></Route>
+          <Route path="/member-person"></Route>
+          <Route path="/member-password"></Route>
+          <Route path="/member-address"></Route>
+          <Route path="/member-address-new"></Route>
+          <Route path="/member-love"></Route>
           <Route path="/member-history"></Route>
-          <Route path="/member-history-detail "></Route>
-          <Route path="/travel-index "></Route>
-          <Route path="/travel-choose "></Route>
-          <Route path="/travel-notes "></Route>
-          <Route path="/ticket-order "></Route>
-          <Route path="/ticket-trip-choose "></Route>
-          <Route path="/ticket-date-choose "></Route>
-          <Route path="/ticket-meal-choose "></Route>
-          <Route path="/ticket-confirm "></Route>
+          <Route path="/member-history-detail"></Route>
+          <Route path="/travel-index"></Route>
+          <Route path="/travel-choose"></Route>
+          <Route path="/travel-notes"></Route>
+          <Route path="/ticket-order"></Route>
+          <Route path="/ticket-trip-choose"></Route>
+          <Route path="/ticket-date-choose"></Route>
+          <Route path="/ticket-meal-choose"></Route>
+          <Route path="/ticket-confirm"></Route>
           <Route path="/coustomize-mainpage">
             <CoustomizeMainPage />
           </Route>
-          <Route path="/coustomize-suit "></Route>
-          <Route path="/coustomize-craft "></Route>
-          <Route path="/coustomize-seat "></Route>
-          <Route path="/coustomize-order "></Route>
-          <Route path="/trans-mainpage "></Route>
-          <Route path="/trans-order "></Route>
-          <Route path="/products-list "></Route>
+          <Route path="/coustomize-suit"></Route>
+          <Route path="/coustomize-craft"></Route>
+          <Route path="/coustomize-seat"></Route>
+          <Route path="/coustomize-order"></Route>
+          <Route path="/trans-mainpage"></Route>
+          <Route path="/trans-order"></Route>
+          <Route path="/products-list"></Route>
           <Route path="/products-detail/:id"></Route>
           <Route path="/products" exact></Route>
-          <Route path="/cart-payment "></Route>
-          <Route path="/cart-information "></Route>
-          <Route path="/cart-final-check "></Route>
-          <Route path="/cart-credit-card "></Route>
-          <Route path="/cart-complete "></Route>
+          <Route exact path="/cart-payment">
+            <CartChoosePayment />
+          </Route>
+          <Route path="/cart-information"></Route>
+          <Route path="/cart-final-check"></Route>
+          <Route path="/cart-credit-card"></Route>
+          <Route path="/cart-complete"></Route>
         </Switch>
         <Footer />
       </>
