6f5bcf95346a5bd0ca52176a50e7820e7ec757c9
Albert <95019007+jack510010@users.noreply.github.com>
2022-03-10 16:16:05 +0800

    修修修補補補

--- src/pages/Cart/CartChoosePayment.js
+++ src/pages/Cart/CartChoosePayment.js
@@ -78,7 +78,32 @@ const CartChoosePayment = () => {
                 <div className="content-detail-inside-bottom">
                   <div className="cart-empty-place"></div>
                   <button className="cart-remove-btn">remove btn</button>
-                  <div className="cart-order-money">NT$XXXXX</div>
+                  <div className="cart-order-money">$XXXXX</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="cart separated-line"></div>
+          <div className="cart cart-order-item d-flex">
+            <div className="cart cart-img-div col-4">
+              <img src={example} alt="" />
+            </div>
+            <div className="cart content-detail col-8 d-flex">
+              <div className="content-detail-inside">
+                <div className="content-detail-inside-top">
+                  <h4 className="cart-product-name">商品名稱</h4>
+                  <select className="cart-select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+                <div className="content-detail-inside-bottom">
+                  <div className="cart-empty-place"></div>
+                  <button className="cart-remove-btn">remove btn</button>
+                  <div className="cart-order-money">$XXXXX</div>
                 </div>
               </div>
             </div>
