eaf2cd0502ec4eaea07c970432f3f75041c7d01f
Albert <95019007+jack510010@users.noreply.github.com>
2022-03-11 20:13:57 +0800

    第一頁RWD告一個段落 80%

--- src/pages/Cart/CartChoosePayment.js
+++ src/pages/Cart/CartChoosePayment.js
@@ -4,6 +4,7 @@ import example from './img/example.png';
 import cartTicket from './img/cartTicket.png';
 import spaceSuit from './img/spaceSuit.png';
 import spaceSeat from './img/spaceSeat.png';
+import spaceShip from './img/spaceShip.png';
 
 const CartChoosePayment = () => {
   return (
@@ -88,7 +89,7 @@ const CartChoosePayment = () => {
           </div>
           <div className="cart separated-line"></div>
           <div className="cart cart-order-item d-flex">
-            <div className="cart cart-img-div col-4">
+            <div className="cart cart-img-div col-4 cart-ticket-margin-y">
               <img src={cartTicket} alt="" />
             </div>
             <div className="cart content-detail col-8 d-flex">
@@ -159,28 +160,29 @@ const CartChoosePayment = () => {
           <div className="cart separated-line"></div>
           <div className="cart cart-order-item d-flex">
             <div className="cart cart-img-div col-4">
-              <img src={cartTicket} alt="" />
+              <img src={spaceShip} alt="" className="spaceship-height" />
             </div>
             <div className="cart content-detail col-8 d-flex">
               <div className="content-detail-inside">
-                <div className="content-detail-inside-top-ticket">
-                  <div className="cart-ticket-destination">
-                    <h5 className="cart-trip-name">行程:泰坦星</h5>
-                    <h5 className="cart-trip-food">太空餐點</h5>
+                <div className="content-detail-inside-top-spaceship">
+                  <div className="cart-spaceship">
+                    <h5 className="cart-spaceship-name">spacecraft</h5>
+                    <h5 className="cart-spaceship-text">鐫刻文字</h5>
                   </div>
-                  <div className="cart-ticket-during">
-                    <h5 className="cart-start-time">啟航 2022/03/10 </h5>
-                    <h5 className="cart-gohome-time">回程 2022/03/15 </h5>
+                  <div className="cart-country-flag">
+                    <div className="cart-empty-flag"></div>
+                    <h5 className="cart-country-flag-text">已選好的國旗</h5>
                   </div>
                 </div>
-                <div className="content-detail-inside-bottom-ticket">
-                  <h5 className="cart-number-people">人數 5</h5>
+                <div className="content-detail-inside-bottom-spaceship">
+                  <div className="cart-number-people"></div>
                   <button className="btn cart-remove-btn">remove</button>
                   <div className="cart-order-money">$XXXXX</div>
                 </div>
               </div>
             </div>
           </div>
+          <div className="cart separated-line"></div>
         </div>
       </div>
     </>
