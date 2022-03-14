842881f9d514482a116e82c944b10478ebf50108
Albert <95019007+jack510010@users.noreply.github.com>
2022-03-14 09:45:57 +0800

    小修改

--- src/pages/Cart/CartChoosePayment.js
+++ src/pages/Cart/CartChoosePayment.js
@@ -12,11 +12,11 @@ const CartChoosePayment = () => {
       <div className="cart-background container-fluid">
         <div className="cart container cart-container">
           <div className="cart row d-flex justify-content-center process-text">
-            <div className="cart step-text cart-ml-6">付款方式</div>
-            <div className="cart step-text cart-ml-6">訂購人資訊</div>
-            <div className="cart step-text cart-ml-6">最後確認</div>
+            <div className="cart step-text">付款方式</div>
+            <div className="cart step-text">訂購人資訊</div>
+            <div className="cart step-text">最後確認</div>
           </div>
-          <div className="cart row d-flex justify-content-center process-line">
+          <div className="cart row d-flex justify-content-around process-line">
             <div className="cart cart-step-line"></div>
             <div className="cart-steps step-one cart-ml-7 d-flex align-items-center justify-content-center">
               1
