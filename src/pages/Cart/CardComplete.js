a1a60aef33d030e519a5c7fafa03dd4eb1889bc7
Albert <95019007+jack510010@users.noreply.github.com>
2022-03-16 20:36:58 +0800

    進度條顏色變化問題想請教大家～

--- src/pages/Cart/CardComplete.js
+++ src/pages/Cart/CardComplete.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import './CardComplete.css';
 
 const CardComplete = () => {
   return (
     <>
-      <div></div>
+      <div className="cart-background container-fluid">
+        <div className="container cart-container">
+          <div className="cart row d-flex justify-content-center process-text">
+            <div className="cart step-text">付款方式</div>
+            <div className="cart step-text">訂購人資訊</div>
+            <div className="cart step-text">最後確認</div>
+          </div>
+          <div className="cart row d-flex justify-content-around process-line">
+            <div className="cart cart-step-line"></div>
+            <div className="cart-steps step-one cart-ml-7 d-flex align-items-center justify-content-center">
+              1
+            </div>
+            <div className="cart-steps step-two cart-ml-7 d-flex align-items-center justify-content-center">
+              2
+            </div>
+            <div className="cart-steps step-three cart-ml-7 d-flex align-items-center justify-content-center">
+              3
+            </div>
+            <div className="cart-steps step-four cart-ml-7 d-flex align-items-center justify-content-center">
+              4
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
