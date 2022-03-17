3e9130a92da04aae97496dbedcd008dcefa3cbf3
Albert <95019007+jack510010@users.noreply.github.com>
2022-03-17 14:52:50 +0800

    第5頁快切完了

--- src/pages/Cart/CardComplete.js
+++ src/pages/Cart/CardComplete.js
@@ -26,6 +26,58 @@ const CardComplete = () => {
               4
             </div>
           </div>
+          <div className="cart d-flex justify-content-center py-5 total-price-text deal-receipt">
+            <p>付款已完成，交易金額為$xxxxx元。</p>
+            <p>謝謝您的訂購，您的訂單編號為：20220129015648。</p>
+          </div>
+
+          {/* 購物明細 */}
+          <div className="cart-list">
+            <div className="payed-list-detail-head">
+              <div className="list-detail-text">購物明細</div>
+            </div>
+
+            {/* 商品明細 */}
+            <div className="deal-receipt-content d-flex flex-column">
+              <div className="deal-receipt-title d-flex align-items-center">
+                <div className="col-5 text-center quantity-detail-border-right">
+                  商品明細
+                </div>
+                <div className="col-4 text-center quantity-detail-border-right">
+                  數量
+                </div>
+                <div className="col-3 text-center">小記</div>
+              </div>
+
+              {/* aaa-衣服 bbb-褲子 */}
+              <div className="product-trip-custom d-flex align-items-center">
+                <div className="col-5 text-center">aaa-衣服</div>
+                <div className="col-4 text-center">1</div>
+                <div className="col-3 text-center">$100</div>
+              </div>
+              <div className="product-trip-custom d-flex align-items-center">
+                <div className="col-5 text-center">bbb-褲子</div>
+                <div className="col-4 text-center">2</div>
+                <div className="col-3 text-center">$400</div>
+              </div>
+
+              {/* 共 2 項商品，數量 3 個，總金額$500  */}
+              <div className="remind-money d-flex align-items-center">
+                <div className="col-5"></div>
+                <div className="col-4 text-center">
+                  共 2 項商品，數量 3 個，總金額
+                </div>
+                <div className="col-3 text-center">$500</div>
+              </div>
+
+              {/* 本訂單已付款金額$500 */}
+              <div className="remind-money-again d-flex align-items-center">
+                <div className="col-5"></div>
+                <div className="col-4 text-center">本訂單已付款金額</div>
+                <div className="col-3 text-center">$500</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
