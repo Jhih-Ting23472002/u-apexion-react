e6973207501f8debad2b65064da9196cdb4baa85
Albert <95019007+jack510010@users.noreply.github.com>
2022-03-21 15:55:11 +0800

    嘗試切component

--- src/pages/Cart/CartChoosePayment.js
+++ src/pages/Cart/CartChoosePayment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './CartChoosePayment.css';
-import example from './img/example.png';
+import ProcessLine from './components/ProcessLine'
+import ProductCard from './components/choosePayment/ProductCard'
 import cartTicket from './img/cartTicket.png';
 import spaceSuit from './img/spaceSuit.png';
 import spaceSeat from './img/spaceSeat.png';
@@ -17,75 +18,13 @@ const CartChoosePayment = (props) => {
             <div className="cart step-text">訂購人資訊</div>
             <div className="cart step-text">最後確認</div>
           </div>
-          <div className="cart row d-flex justify-content-around process-line">
-            <div className="cart cart-step-line"></div>
-            <div className="cart-steps step-one cart-ml-7 d-flex align-items-center justify-content-center">
-              1
-            </div>
-            <div className="cart-steps step-two cart-ml-7 d-flex align-items-center justify-content-center">
-              2
-            </div>
-            <div className="cart-steps step-three cart-ml-7 d-flex align-items-center justify-content-center">
-              3
-            </div>
-            <div className="cart-steps step-four cart-ml-7 d-flex align-items-center justify-content-center">
-              4
-            </div>
-          </div>
+          <ProcessLine />
           <div className="cart d-flex justify-content-center py-5 total-price-text">
             您的購物袋裡有以下商品，目前總金額是 NT$xxxxxx。
           </div>
 
-          <div className="cart separated-line"></div>
-          <div className="cart cart-order-item d-flex">
-            <div className="cart cart-img-div col-4">
-              <img src={example} alt="" />
-            </div>
-            <div className="cart content-detail col-8 d-flex">
-              <div className="content-detail-inside">
-                <div className="content-detail-inside-top">
-                  <h5 className="cart-product-name">商品名稱</h5>
-                  <select className="cart-select">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </select>
-                </div>
-                <div className="content-detail-inside-bottom">
-                  <div className="cart-empty-place"></div>
-                  <button className="btn cart-remove-btn">remove</button>
-                  <div className="cart-order-money">$xxxxx</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="cart separated-line"></div>
-          <div className="cart cart-order-item d-flex">
-            <div className="cart cart-img-div col-4">
-              <img src={example} alt="" />
-            </div>
-            <div className="cart content-detail col-8 d-flex">
-              <div className="content-detail-inside">
-                <div className="content-detail-inside-top">
-                  <h5 className="cart-product-name">商品名稱</h5>
-                  <select className="cart-select">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </select>
-                </div>
-                <div className="content-detail-inside-bottom">
-                  <div className="cart-empty-place"></div>
-                  <button className="btn cart-remove-btn">remove</button>
-                  <div className="cart-order-money">$XXXXX</div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <ProductCard />
+          
           <div className="cart separated-line"></div>
           <div className="cart cart-order-item d-flex">
             <div className="cart cart-img-div col-4 cart-ticket-margin-y">
