6694ff2e0ca0fb480880bda4666f7c345a94fb77
Jhih Ting <tina23472002@gmail.com>
2022-03-22 16:56:34 +0800

    詳細頁推薦商品

--- src/pages/Products/components/DetailRecommend.js
+++ src/pages/Products/components/DetailRecommend.js
@@ -1,38 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import '../productsDetail.scss';
 import ProductsConfig from '../ProductsConfig';
+import { Link } from 'react-router-dom';
 
 function DetailRecommend(props) {
-   const { product, ProductDetail } = props
-  console.log(props)
-  // console.log(props.product.category)
+  const { product, ProductDetail } = props;
+  // console.log(props);
 
-  const a= props.ProductDetail.filter((v, i) => {
-            if (props.product.category == v.category) {
-              return v;
-            }
-          }) 
-          console.log(a)
+  const a = props.ProductDetail.filter((v, i) => {
+    if (props.product.category == v.category && product.sid !== v.sid) {
+      return v ; 
+    }
+  });
+  // console.log(a);
+  const b = a.filter((v, i) => {
+    if (props.product.class == v.class) {
+      return v ;
+    }
+  });
+  const c = b.slice(0, 3);
+  // console.log(c);
 
   return (
     <>
-      <div className="col-md-4">
-        <div className="card pr-card mb-5">
-          <img
-            className="card-img-top"
-            src="./product_img/M021J_BK019.jpg"
-            alt=""
-          />
-          <div className="card-body">
-            <p className="card-text mb-3">
-              {props.ProductDetail?.product_name ?? '重新整理'}
-            </p>
-            <p className="card-text">
-              {props.ProductDetail?.material ?? '灰色科技棉防撕裂物料'}
-            </p>
+      {c.map(v => (
+        <div className="col-md-4">
+          <div className="card pr-card mb-5">
+            <img
+              className="card-img-top"
+              src={ProductsConfig.IMG_PATH + '/' + v.product_img}
+              alt=""
+            />
+            <div className="card-body">
+              <p className="card-text mb-3">{v.product_name}</p>
+              <p className="card-text">{v.material}</p>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </>
   );
 }
