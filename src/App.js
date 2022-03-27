8537eb11d26c210f64c1201e74d149265973ac89
Jhih Ting <tina23472002@gmail.com>
2022-03-27 13:35:02 +0800

    引入變更

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/navbar';
@@ -11,8 +11,8 @@ import ScrollToTop from './pages/Cart/components/ScrollToTop';
 import NotFoundPage from './pages/NotFoundPage';
 
 // Member
-import MemberLogin from './pages/Member/MemberLogin';
-import MemberSignUp from './pages/Member/MemberSignUp';
+ import MemberLogin from './pages/Member/MemberLogin';
+ import MemberSignUp from './pages/Member/MemberSignUp';
 import MemberForgotPwd from './pages/Member/MemberForgotPwd';
 import MemberPerson from './pages/Member/MemberPerson';
 import MemberPersonEdit from './pages/Member/MemberPersonEdit';
