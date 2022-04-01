543e0b04eba2a73b2fc3eb726c4fd31fe55cf6c5
Sam Wang <95230821+HSINCHUNWANG@users.noreply.github.com>
2022-04-01 17:09:48 +0800

    member update

--- src/pages/Member/MemberPerson.js
+++ src/pages/Member/MemberPerson.js
@@ -207,17 +207,14 @@ const MemberPerson = () => {
                 <div className="person-line"></div>
               </div>
               <div className="person-avatar-container">
-                <div className="person-avatar-img">
-                  <img
-                    className="person-avatar-img"
-                    //如果沒有上傳照片??，就給預設的照片
-                    src={`http://localhost:3001/img/${
-                      memInfo.avatar ??
-                      'bdeb89f5-bcd8-4261-b721-3ec0ce4889db.jpg'
-                    }`}
-                    alt=""
-                  />
-                </div>
+                <img
+                  className="person-avatar-img"
+                  //如果沒有上傳照片??，就給預設的照片
+                  src={`http://localhost:3001/img/${
+                    memInfo.avatar ?? 'bdeb89f5-bcd8-4261-b721-3ec0ce4889db.jpg'
+                  }`}
+                  alt=""
+                />
               </div>
             </div>
           </div>
