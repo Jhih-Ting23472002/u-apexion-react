c79fca4dcc9ea067893861f9f8013957ca36e219
Jhih Ting <tina23472002@gmail.com>
2022-04-05 21:31:06 +0800

    影片串接

--- src/pages/Cart/CartCreditCardModal.js
+++ src/pages/Cart/CartCreditCardModal.js
@@ -3,42 +3,27 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import ReactPlayer from 'react-player';
 import './CartCreditCardModal.css';
+import '../../../node_modules/react-modal-video/scss/modal-video.scss';
+import ModalVideo from 'react-modal-video'
 
 // import './CartCreditCardModal.css';
 
-function CartCreditCardModal({ show, setShow, handleClose, handleShow }) {
+function CartCreditCardModal({cartConfirmModalShow, setCartConfirmModalShow}) {
   return (
     <>
-      <Modal
-        size="lg"
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-        className="credit-video-end-wrap"
-      >
-        {/* <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header> */}
-        <Modal.Body className="credit-video-end">
-          {/* <p>12345678</p> */}
-          <div className="video-end">
+<React.Fragment >
+			<ModalVideo channel='custom' autoplay isOpen={cartConfirmModalShow} url="./end.mp4" onClose={() => setCartConfirmModalShow(false)}/>
+		</React.Fragment>
+          {/* <div className="video-end">
             <ReactPlayer
               id="video-end-player"
               url="./end.mp4"
-              // width="100%"
-              // height="100%"
+              width="100%"
+              height="100%"
               playing={true}
             />
-          </div>
-        </Modal.Body>
-        {/* <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer> */}
-      </Modal>
+          </div> */}
+
     </>
   );
 }
