951d1745d12eba9d3b5384ff5b5d54c2e354f0df
Jhih Ting <tina23472002@gmail.com>
2022-04-10 14:20:59 +0800

    購物車光箱

--- src/pages/Cart/CartCreditCardModal.js
+++ src/pages/Cart/CartCreditCardModal.js
@@ -4,13 +4,14 @@ function CartCreditCardModal(props) {
   return (
     <Modal
       {...props}
-      size="lg"
+      size="sm"
       aria-labelledby="contained-modal-title-vcenter"
       centered
       // style={{ border: '1px solid red' }}
     >
       <Modal.Header closeButton>
         <Modal.Title
+          style={{ color: 'white', margin: 'auto',fontSize: '30px' }}
           id="contained-modal-title-vcenter"
           // style={{ border: '1px solid red' }}
         >
@@ -18,14 +19,16 @@ function CartCreditCardModal(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p style={{ fontSize: '16px' }}>期待在太空與您相遇</p>
+        <p style={{ fontSize: '16px', color: 'white', textAlign: 'center' }}>
+          期待在太空與您相遇
+        </p>
       </Modal.Body>
       <Modal.Footer>
         <Button
           onClick={props.onHide}
           style={{
             backgroundColor: '#05f2f2',
-            color: 'grey',
+            color: 'black',
             fontSize: '14px',
             fontWeight: '500',
             letterSpacing: '2px',
